b6c2c6b91890dbf963f011a8df7fbcd3e57deb02
Volatile Man <>
2022-07-19 11:12:25 +0300

    Remove speed limit

--- connect-vpn.sh
+++ connect-vpn.sh
@@ -6,9 +6,3 @@ nmcli c down $CONNECTION_NAME
 sleep 25
 nmcli c up $CONNECTION_NAME
 
-currenttime=$(date +%H:%M)
-if  [[ "$currenttime" < "01:30" ]] || [[ "$currenttime" > "07:30" ]]; then
-    if  [[ $(date +%u) -le 5 ]]; then
-        /sbin/wondershaper tun0 4000 4000
-    fi
-fi
