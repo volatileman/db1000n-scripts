cd3eb1d3dedabb816fc50b1dba9c5aeaed86f204
Volatile Man <>
2022-07-23 21:15:56 +0300

    Add attemting to vpn connection

--- connect-vpn.sh
+++ connect-vpn.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
 CONNECTION_NAME="us.protonvpn.net.tcp"
+MAX_ATTEMPTS=3
+ATEMPT=1
 
 nmcli c down $CONNECTION_NAME
-sleep 25
-nmcli c up $CONNECTION_NAME
 
+while [ $ATEMPT -le $MAX_ATTEMPTS ]
+do
+    sleep 20
+    
+    if nmcli c up $CONNECTION_NAME; then
+        break
+    fi
+    
+    ((ATEMPT++))
+done;
